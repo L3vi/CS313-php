6eaf44de43263e81655d13c4f117be18e0a0c4a9
L3vi <levistum@gmail.com>
2018-09-30 16:32:07 -0600

    One more fix?...

--- web/header.php
+++ web/header.php
@@ -1,18 +1,15 @@
 <?php
-	$fileName = pathinfo($_SERVER['PHP_SELF'], PATHINFO_EXTENSION);
-	echo $fileName;
-	$file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_BASENAME - PATHINFO_EXTENSION);
-	echo $file;
+	$fileName = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 ?>
 
 <ul class="nav nav-tabs">
 	<li class="nav-item">
-		<a class="nav-link" href="home.php">Home</a>
+		<a class="nav-link <?php if ($fileName === 'home') echo 'active' ?>" href="home.php">Home</a>
 	</li>
 	<li class="nav-item">
-		<a class="nav-link" href="about-us.php">About Us</a>
+		<a class="nav-link <?php if ($fileName === 'about-us') echo 'active' ?>" href="about-us.php">About Us</a>
 	</li>
 	<li class="nav-item">
-		<a class="nav-link" href="login.php">Login</a>
+		<a class="nav-link <?php if ($fileName === 'login') echo 'active' ?>" href="login.php">Login</a>
 	</li>
 </ul>
