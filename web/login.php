a281e365e8e9d8dbbc042a60ba8ea46b87b7f745
L3vi <levistum@gmail.com>
2018-09-30 16:12:00 -0600

    Updated headers

--- web/login.php
+++ web/login.php
@@ -2,12 +2,13 @@
 <html>
 <head>
 	<title>Login</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <?php
 	require("header.php");
 ?>
 <body>
-	
+
 	<button type="button">Admin Login</button>
 	<button type="button">Tester Login</button>
 	<?php 
