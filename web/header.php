4513a57280e40ccc07fae1e45063280472a3b03a
L3vi <levistum@gmail.com>
2018-09-30 16:09:23 -0600

    Added header for realz

---
+++ web/header.php
@@ -0,0 +1,11 @@
+<ul class="nav nav-tabs">
+	<li class="nav-item">
+		<a class="nav-link active" href="home.php">Home</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" href="about-us.php">About Us</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" href="login.php">Login</a>
+	</li>
+</ul>
