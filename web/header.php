68cdf80cb5d53271b2620e2c53b28d353f6617e5
L3vi <levistum@gmail.com>
2018-09-30 16:27:36 -0600

    One fix at a time...

--- web/header.php
+++ web/header.php
@@ -2,7 +2,7 @@
 
 <ul class="nav nav-tabs">
 	<li class="nav-item">
-		<a class="nav-link active" href="home.php">Home</a>
+		<a class="nav-link" href="home.php">Home</a>
 	</li>
 	<li class="nav-item">
 		<a class="nav-link" href="about-us.php">About Us</a>
