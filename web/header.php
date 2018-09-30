b847fffe275cc676a119b69a37de696f7a84e529
L3vi <levistum@gmail.com>
2018-09-30 16:25:34 -0600

    Fixed some php in the header.

--- web/header.php
+++ web/header.php
@@ -1,9 +1,10 @@
-<?php
+<!-- <?php
 	$fileName = pathinfo($_SERVER['PHP_SELF'], PATHINFO_EXTENSION);
 	echo $fileName
 	$file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_BASENAME - PATHINFO_EXTENSION);
 	echo $file
-?>
+?> -->
+
 <ul class="nav nav-tabs">
 	<li class="nav-item">
 		<a class="nav-link" href="home.php">Home</a>
