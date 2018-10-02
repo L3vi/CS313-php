756a77a5a9116898c3bca70771c4d6401a6f362c
L3vi <levistum@gmail.com>
2018-10-02 15:42:32 -0600

    emergency updated 2

---
+++ web/assignments.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<?php
+	require("header.php");
+?>
+<body>
+
+	<h1>Coming Soon...</h1>
+	<?php 
+
+	?>
+</body>
+</html>
