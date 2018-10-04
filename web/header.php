7e4438f2ac60084c2b600a32ac42ac78ddbb222e
L3vi <levistum@gmail.com>
2018-10-03 21:22:42 -0600

    "Updates..."

--- web/header.php
+++ web/header.php
@@ -2,7 +2,7 @@
 	$fileName = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 ?>
 
-<ul class="navbar navbar-dark bg-dark">
+<ul class="navbar navbar-dark bg-dark justify-content-start">
 		<a class="nav-link <?php if ($fileName === 'home') echo 'active' ?>" href="home.php">Home</a>
 		<a class="nav-link <?php if ($fileName === 'assignments') echo 'active' ?>" href="assignments.php">Assignments</a>
 		<a class="nav-link" target="_blank" href="https://l3vi.github.io/index.html">Other Projects</a>
