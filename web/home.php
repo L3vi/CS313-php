b4fe2b8093f7d0980187b85dc23a5ea4066acbd9
L3vi <levistum@gmail.com>
2018-10-04 16:23:37 -0600

    "Updates..."

--- web/home.php
+++ web/home.php
@@ -33,13 +33,7 @@ if (isset($_SESSION["timesVisited"])) {
 		<input type="submit">
 	</form>
 	<?php 
-	echo "You've visited this website $_SESSION["timesVisited"]";
-	// if ($_SESSION["timesVisited"] > 30) {
-	// 	echo "You should probably go now...";
-	// }
-	// if ($_SESSION["timesVisited"] > 75) {
-	// 	echo "But like really... go away!";
-	// } 
+	echo $_SESSION["timesVisited"];
 	?>
 </body>
 </html>
