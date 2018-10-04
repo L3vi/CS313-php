ecbb0a612fee03a53b41880840b3dc637966643d
L3vi <levistum@gmail.com>
2018-10-04 16:02:12 -0600

    "Updates..."

---
+++ web/cookiesResult.php
@@ -0,0 +1,16 @@
+<?php
+
+$_COOKIE["fav-nug"];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php 
+	echo "Information about my favorite chicken nuggets: $fav-nug";
+	?>
+</body>
+</html>
