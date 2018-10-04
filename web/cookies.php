f3a2103db8d03054484c5f4151d26ba7a1a3c54a
L3vi <levistum@gmail.com>
2018-10-04 15:57:11 -0600

    "Updates..."

---
+++ web/cookies.php
@@ -0,0 +1,17 @@
+<?php 
+
+setcookie("fav-nug", "My favorite chicken nugget is Chik Fil A", time() + (86400 * 1));
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<p>This is a page... n' stuff</p>
+
+</body>
+</html>
