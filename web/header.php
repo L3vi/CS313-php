06d625cfdc56cc85bea85ee513f16eae447e573a
L3vi <levistum@gmail.com>
2018-10-03 21:00:04 -0600

    "Updates..."

--- web/header.php
+++ web/header.php
@@ -3,13 +3,7 @@
 ?>
 
 <ul class="navbar navbar-dark bg-dark">
-	<li class="nav-item">
 		<a class="nav-link <?php if ($fileName === 'home') echo 'active' ?>" href="home.php">Home</a>
-	</li>
-	<li class="nav-item">
 		<a class="nav-link <?php if ($fileName === 'assignments') echo 'active' ?>" href="assignments.php">Assignments</a>
-	</li>
-	<li class="nav-item">
 		<a class="nav-link" target="_blank" href="https://l3vi.github.io/index.html">Other Projects</a>
-	</li>
 </ul>
