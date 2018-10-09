1db0578bf2f67f8119ef79e643e6dc5c56d6428f
L3vi <levistum@gmail.com>
2018-10-09 15:55:07 -0600

    Added basic structure for store

---
+++ web/shoppingCart/confirmation.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Confirmation</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="styling.css" type="text/css">
+</head>
+<?php
+	require("header.php");
+?>
+<body>
+	<?php 
+	echo "You will shop here.";
+
+	?>
+
+</body>
+</html>
