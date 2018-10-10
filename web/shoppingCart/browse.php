8c74d1a084653393d4b158396397d7cbcee82426
L3vi <levistum@gmail.com>
2018-10-10 15:38:19 -0600

    "Updates..."

--- web/shoppingCart/browse.php
+++ web/shoppingCart/browse.php
@@ -6,37 +6,44 @@
 	<link rel="stylesheet" href="shopStyle.css" type="text/css">
 </head>
 <?php
-	require("header.php");
+require("header.php");
 ?>
 <body>
 	<form>
-	<h1 class="display-1 text-center">Nuggs before Drugs</h1>
+		<h1 class="display-1 text-center">Nuggs before Drugs</h1>
 
-	<div class="container text-center">
-<!-- Row 1 -->
-	  <div class="row">
-	    <div class="col-lg">
-	    	<img src="images/burgerKing.jpg"><br/>
-	      <figcaption class="figure-caption"><button type="button" class="btn btn-outline-secondary">-</button>Burger King<button type="button" class="btn btn-outline-secondary">+</button></figcaption>
-	    </div>
-	    <div class="col-lg">
-	    	<img src="images/chickFilA.jpg"><br/>
-	      <figcaption class="figure-caption"><button type="button" class="btn btn-outline-secondary">-</button>Chik-Fil-A<button type="button" class="btn btn-outline-secondary">+</button></figcaption>
-	    </div>
-	  </div>
-<!-- Row 2 -->
-	  <div class="row">
-	    <div class="col-lg">
-	    	<img src="images/mcDonalds.jpg"><br/>
-	    	<figcaption class="figure-caption">McDonalds</figcaption>
-	    </div>
-	    <div class="col-lg">
-	    	<img src="images/wendys.jpg"><br/>
-	      <figcaption class="figure-caption">Wendy's</figcaption>
-	    </div>
-	  </div>
+		<div class="container text-center">
+			<!-- Row 1 -->
+			<div class="row">
+				<div class="col-lg">
+					<img src="images/burgerKing.jpg"><br/>
 
-	</div>
+					<figcaption class="figure-caption">
+						<button type="button" class="btn btn-outline-secondary">-</button>
+						 Burger King 
+						<button type="button" class="btn btn-outline-secondary">+</button>
+					</figcaption>
+				</div>
+				<div class="col-lg">
+					<img src="images/chickFilA.jpg"><br/>
+					<figcaption class="figure-caption"><button type="button" class="btn btn-outline-secondary">-</button>
+						 Chik-Fil-A 
+					 <button type="button" class="btn btn-outline-secondary">+</button></figcaption>
+				</div>
+			</div>
+			<!-- Row 2 -->
+			<div class="row">
+				<div class="col-lg">
+					<img src="images/mcDonalds.jpg"><br/>
+					<figcaption class="figure-caption"> McDonalds </figcaption>
+				</div>
+				<div class="col-lg">
+					<img src="images/wendys.jpg"><br/>
+					<figcaption class="figure-caption"> Wendy's </figcaption>
+				</div>
+			</div>
+
+		</div>
 	</form>
 
 </body>
