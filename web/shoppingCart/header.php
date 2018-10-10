9709bf7b5f2df797148f5b0734274c846fc0d9fc
L3vi <levistum@gmail.com>
2018-10-09 23:12:29 -0600

    "Updates..."

--- web/shoppingCart/header.php
+++ web/shoppingCart/header.php
@@ -4,7 +4,7 @@
 
 <ul class="navbar navbar-dark bg-dark justify-content-start">
 		<a class="nav-link <?php if ($fileName === 'home') echo 'active' ?>" href="../home.php">&larr;Back</a>
-		<a class="nav-link <?php if ($fileName === 'browse') echo 'active' ?>" href="browse.php">Browse</a>
+		<a class="nav-link <?php if ($fileName === 'browse') echo 'active' ?>" href="browse.php"><?php echo "$fileName" ?></a>
 		<a class="nav-link <?php if ($fileName === 'cart') echo 'active' ?>" href="cart.php">Cart</a>
 		<a class="nav-link <?php if ($fileName === 'checkout') echo 'active' ?>" href="checkout.php">Checkout</a>
 </ul>
