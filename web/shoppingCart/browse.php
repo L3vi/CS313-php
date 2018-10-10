3c513cb2b7c66c49e21dc635d78b09e490d6cc9b
L3vi <levistum@gmail.com>
2018-10-10 11:49:57 -0600

    "Updates..."

--- web/shoppingCart/browse.php
+++ web/shoppingCart/browse.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Browse</title>
+	<title>Browse the nuggs</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="shopStyle.css" type="text/css">
 </head>
@@ -13,29 +13,25 @@
 	  <div class="row">
 	    <div class="col-lg">
 	    	<img src="images/burgerKing.jpg"><br/>
-	      <figcaption class="figure-caption text-right">Burger King</figcaption>
+	      <figcaption class="figure-caption">Burger King</figcaption>
 	    </div>
 	    <div class="col-lg">
 	    	<img src="images/chickFilA.jpg"><br/>
-	      <figcaption class="figure-caption text-right">Chik-Fil-A</figcaption>
+	      <figcaption class="figure-caption">Chik-Fil-A</figcaption>
 	    </div>
 	  </div>
 	  <div class="row">
 	    <div class="col-lg">
 	    	<img src="images/mcDonalds.jpg"><br/>
-	    	<figcaption class="figure-caption text-right">McDonalds</figcaption>
+	    	<figcaption class="figure-caption">McDonalds</figcaption>
 	    </div>
 	    <div class="col-lg">
 	    	<img src="images/wendys.jpg"><br/>
-	      <figcaption class="figure-caption text-right">Wendy's</figcaption>
+	      <figcaption class="figure-caption">Wendy's</figcaption>
 	    </div>
 	  </div>
 
 	</div>
-	<?php 
-	echo "You will browse here.";
-
-	?>
 
 </body>
 </html>
--- web/shoppingCart/cart.php
+++ web/shoppingCart/cart.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Cart</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link rel="stylesheet" href="styling.css" type="text/css">
+	<link rel="stylesheet" href="shopStyle.css" type="text/css">
 </head>
 <?php
 	require("header.php");
