27f21afd444b51f5d542f9e1383f27810b844100
L3vi <levistum@gmail.com>
2018-10-10 12:01:55 -0600

    "Updates..."

--- web/shoppingCart/browse.php
+++ web/shoppingCart/browse.php
@@ -12,7 +12,7 @@
 	<h1 class="display-1 text-center">Nuggs before Drugs</h1>
 	<div class="container">
 	  <div class="row">
-	    <div class="col-lg">
+	    <div class="col-lg text-center">
 	    	<img src="images/burgerKing.jpg"><br/>
 	      <figcaption class="figure-caption">Burger King</figcaption>
 	    </div>
