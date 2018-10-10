dc6105c64d8b6de951f8f1c5e0b3648afba47554
L3vi <levistum@gmail.com>
2018-10-10 12:28:52 -0600

    "Updates..."

--- web/shoppingCart/browse.php
+++ web/shoppingCart/browse.php
@@ -17,11 +17,11 @@
 	  <div class="row">
 	    <div class="col-lg">
 	    	<img src="images/burgerKing.jpg"><br/>
-	      <figcaption class="figure-caption"><button type="button" class="btn btn-outline-light">-</buttonn>Burger King<button type="button" class="btn btn-outline-light">+</buttonn></figcaption>
+	      <figcaption class="figure-caption"><button type="button" class="btn btn-outline-light">-</button>Burger King<button type="button" class="btn btn-outline-light">+</button></figcaption>
 	    </div>
 	    <div class="col-lg">
 	    	<img src="images/chickFilA.jpg"><br/>
-	      <figcaption class="figure-caption"><button type="button" class="btn btn-outline-light">-</buttonn>Chik-Fil-A<button type="button" class="btn btn-outline-light">+</buttonn></figcaption>
+	      <figcaption class="figure-caption"><button type="button" class="btn btn-outline-light">-</button>Chik-Fil-A<button type="button" class="btn btn-outline-light">+</button></figcaption>
 	    </div>
 	  </div>
 <!-- Row 2 -->
