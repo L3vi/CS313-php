301801ecae7fce53efeb100eac572e215ed969c4
L3vi <levistum@gmail.com>
2018-10-10 12:27:21 -0600

    "Updates..."

--- web/shoppingCart/browse.php
+++ web/shoppingCart/browse.php
@@ -9,18 +9,22 @@
 	require("header.php");
 ?>
 <body>
+	<form>
 	<h1 class="display-1 text-center">Nuggs before Drugs</h1>
+
 	<div class="container text-center">
+<!-- Row 1 -->
 	  <div class="row">
 	    <div class="col-lg">
 	    	<img src="images/burgerKing.jpg"><br/>
-	      <figcaption class="figure-caption">Burger King</figcaption>
+	      <figcaption class="figure-caption"><button type="button" class="btn btn-outline-light">-</buttonn>Burger King<button type="button" class="btn btn-outline-light">+</buttonn></figcaption>
 	    </div>
 	    <div class="col-lg">
 	    	<img src="images/chickFilA.jpg"><br/>
-	      <figcaption class="figure-caption">Chik-Fil-A</figcaption>
+	      <figcaption class="figure-caption"><button type="button" class="btn btn-outline-light">-</buttonn>Chik-Fil-A<button type="button" class="btn btn-outline-light">+</buttonn></figcaption>
 	    </div>
 	  </div>
+<!-- Row 2 -->
 	  <div class="row">
 	    <div class="col-lg">
 	    	<img src="images/mcDonalds.jpg"><br/>
@@ -33,6 +37,7 @@
 	  </div>
 
 	</div>
+	</form>
 
 </body>
 </html>
