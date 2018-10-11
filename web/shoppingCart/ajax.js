296fd5a2c0c677f3e50be2fbf371c0a81a930b16
L3vi <levistum@gmail.com>
2018-10-10 21:03:12 -0600

    "Updates..."

--- web/shoppingCart/ajax.js
+++ web/shoppingCart/ajax.js
@@ -3,11 +3,11 @@ var chickFilA = 0;
 var mcDonalds = 0;
 var wendys = 0;
 
-function increment() {
-  console.log("test");
+function increment(this) {
   console.log(this);
-  console.log(this.name);
   console.log(this.value);
+  console.log(this.name);
+
 };
 
   // $.ajax({
