9ec9327febc6bb1af63dba509431aeb99f009372
L3vi <levistum@gmail.com>
2018-10-10 21:35:40 -0600

    "Updates..."

--- web/shoppingCart/ajax.js
+++ web/shoppingCart/ajax.js
@@ -7,7 +7,7 @@ var buttons = document.getElementsByClassName("btn-increment");
 var i = 0;
 for(i; i < buttons.length; i++) {
   buttons[i].addEventListener("click", (e) => {
-    console.log("Test");
+    console.log(e);
   });
 }
 
