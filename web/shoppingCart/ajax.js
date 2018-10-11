0ef512cd1051225609366a9121bb8c841367ff5c
L3vi <levistum@gmail.com>
2018-10-10 19:36:54 -0600

    "Updates..."

--- web/shoppingCart/ajax.js
+++ web/shoppingCart/ajax.js
@@ -1,5 +1,27 @@
+var burgerKing = 0;
+var ChickFilA = 0;
+var McDonalds = 0;
+var Wendys = 0;
 function changeQuantity(product) {
-  alert(product);
+  switch(product) {
+    case "burgerKing": 
+    burgerKing++;
+    console.log(burgerKing);
+    break;
+    case "ChickFilA":
+    ChickFilA++;
+    console.log(ChickFilA);
+    break;
+    case "McDonalds":
+    McDonalds++;
+    console.log(McDonalds);
+    break;
+    case "Wendys":
+    Wendys++;
+    console.log(Wendys);
+    break;
+  };
+
   $.ajax({
     url: "cart.php",
     type: "POST",
