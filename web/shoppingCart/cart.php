a596fee2fd59e064f1490b75cab446914665f7e3
L3vi <levistum@gmail.com>
2018-10-10 22:19:07 -0600

    "Updates..."

--- web/shoppingCart/cart.php
+++ web/shoppingCart/cart.php
@@ -1,6 +1,5 @@
 <?php
-	$burgerKing = $_POST['burgerKing'];
-	$shoppingList = $_POST['cart'];
+$shoppingList = $_POST['cart'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,14 +9,38 @@
 	<link rel="stylesheet" href="shopStyle.css" type="text/css">
 </head>
 <?php
-	require("header.php");
+require("header.php");
 ?>
 <body>
+	<ul class="list-group">
+		<li class="list-group-item">Cras justo odio</li>
+		<li class="list-group-item">Dapibus ac facilisis in</li>
+		<li class="list-group-item">Morbi leo risus</li>
+		<li class="list-group-item">Porta ac consectetur ac</li>
+		<li class="list-group-item">Vestibulum at eros</li>
+	</ul>
 	<?php 
-	echo "This is the cart.";
+	echo "<ul class="list-group">"
 	foreach ($shoppingList as $item) {
-		echo $item;
+		echo "<li class=\"list-group-item\">$item</li>"
+		// switch($item) {
+		// 	case "burgerKing":
+		// 		echo "<li class=\"list-group-item\">$item</li>"
+		// 		break;
+		// 	case "chickFilA":
+		// 		echo "<li class=\"list-group-item\">$item</li>"
+		// 		break;
+		// 	case "mcDonalds":
+		// 		echo "<li class=\"list-group-item\">$item</li>"
+		// 		break;
+		// 	case "wendys":
+		// 		echo "<li class=\"list-group-item\">$item</li>"
+		// 		break;
+		// 	default:
+		// 		break;
+		// }
 	}
+	echo "</ul";
 	?>
 
 </body>
