08909d15974d8070e068a8376ba956d3b7791d36
L3vi <levistum@gmail.com>
2018-10-12 17:43:59 -0600

    "Updates..."

--- web/shoppingCart/updateCart.php
+++ web/shoppingCart/updateCart.php
@@ -2,21 +2,30 @@
 
 $item = $_POST['id'];
 $willIncrement = $_POST['willIncrement'];
+echo $item;
+echo $willIncrement;
+exit;
 
 switch ($item) {
-		case 'bk':
-			$willIncrement ? $burgerKingQuantity++ : $burgerKingQuantity--;
-			break;
-		case 'cf':
-			$willIncrement ? $chickFilAQuantity++ : $chickFilAQuantity--;
-			break;
-		case 'md':
-			$willIncrement ? $mcDonaldsQuantity++ : $mcDonaldsQuantity--;
-			break;
-		case 'w':
-			$willIncrement ? $wendysQuantity++ : $wendysQuantity--;
-			break;
-		default:
-			break;
-	};
+	case 'bk':
+	$willIncrement ? $burgerKingQuantity++ : $burgerKingQuantity--;
+	break;
+
+	case 'cf':
+	$willIncrement ? $chickFilAQuantity++ : $chickFilAQuantity--;
+	break;
+
+	case 'md':
+	$willIncrement ? $mcDonaldsQuantity++ : $mcDonaldsQuantity--;
+	break;
+
+	case 'w':
+	$willIncrement ? $wendysQuantity++ : $wendysQuantity--;
+	break;
+
+	default:
+	break;
+};
+
+
 ?>
