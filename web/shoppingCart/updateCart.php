1e7122c81ffabb4072ae6d3ff578a9016ab3de01
L3vi <levistum@gmail.com>
2018-10-12 19:35:54 -0600

    "Updates..."

--- web/shoppingCart/updateCart.php
+++ web/shoppingCart/updateCart.php
@@ -6,22 +6,22 @@ $products = $_SESSION['products'];
 foreach ($products as $product) {
 	switch ($product->name) {
 		case "Burger King Nuggs":
-		var_dump($_SESSION);
+		echo "Burger King";
 		break;
 
 		case 'Chik-Fil-A Nuggs':
 		$willIncrement ? $chickFilANug->quantity++ : $chickFilANug->quantity--;
-		var_dump($chickFilANug);
+		echo "Chik-Fil-A";
 		break;
 
 		case 'McDonalds Nuggs':
 		$willIncrement ? $mcDonaldsNug->quantity++ : $mcDonaldsNug->quantity--;
-		echo $mcDonaldsNug;
+		echo "McDonalds";
 		break;
 
 		case 'Wendy\'s Nuggs':
 		$willIncrement ? $wendysNug->quantity++ : $wendysNug->quantity--;
-		echo $wendysNug;
+		echo "Wendy's";
 		break;
 
 		default:
