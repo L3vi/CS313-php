edcc3277f856cb96b9431046340356bc44384033
L3vi <levistum@gmail.com>
2018-10-12 19:09:35 -0600

    "Updates..."

--- web/shoppingCart/browse.php
+++ web/shoppingCart/browse.php
@@ -31,7 +31,7 @@ if(!isset($_SESSION['products'])) {
 
 	$_SESSION['product'] = "Test";
 };
-var_dump($_SESSION);
+// var_dump($_SESSION);
 
 ?>
 <!DOCTYPE html>
--- web/shoppingCart/updateCart.php
+++ web/shoppingCart/updateCart.php
@@ -1,26 +1,27 @@
 <?php
 session_start();
 echo $_SESSION['products'][0];
+var_dump($_SESSION);
 
-switch ($item) {
-	case 'bk':
-	$willIncrement ? $_SESSION['products'][0]->quantity++ : $$_SESSION['products'][0]->quantity--;
-	break;
+// switch ($item) {
+// 	case 'bk':
+// 	$willIncrement ? $burgerKingQuantity++ : $burgerKingQuantity--;
+// 	break;
 
-	case 'cf':
-	$willIncrement ? $chickFilAQuantity++ : $chickFilAQuantity--;
-	break;
+// 	case 'cf':
+// 	$willIncrement ? $chickFilAQuantity++ : $chickFilAQuantity--;
+// 	break;
 
-	case 'md':
-	$willIncrement ? $mcDonaldsQuantity++ : $mcDonaldsQuantity--;
-	break;
+// 	case 'md':
+// 	$willIncrement ? $mcDonaldsQuantity++ : $mcDonaldsQuantity--;
+// 	break;
 
-	case 'w':
-	$willIncrement ? $wendysQuantity++ : $wendysQuantity--;
-	break;
+// 	case 'w':
+// 	$willIncrement ? $wendysQuantity++ : $wendysQuantity--;
+// 	break;
 
-	default:
-	break;
-};
+// 	default:
+// 	break;
+// };
 
 ?>
