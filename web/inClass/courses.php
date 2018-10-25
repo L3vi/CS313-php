57ea1d5a648f840147a83630e8211451e0714c94
L3vi <levistum@gmail.com>
2018-10-25 16:12:11 -0600

    "Updates..."

---
+++ web/inClass/courses.php
@@ -0,0 +1,29 @@
+<?php
+	require("dbConnect.php");
+	$db = get_db();
+
+	$query = 'SELECT id, code, name FROM course';
+	$statement = $db->prepare($query);
+	$statement->execute;
+	$courses = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Courses</title>
+</head>
+<body>
+	<h1>Courses</h1>
+
+	<ul>
+		<?php
+			$foreach ($courses as $course) {
+				$id = $course['id'];
+				$name = $course['name'];
+				$code = $course['code'];
+				echo "<li>" . $name . "</li>";
+			}
+		?>
+	</ul>
+</body>
+</html>
