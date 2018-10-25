f284e67acfcdff5bf9c778c538f9385ae1e0d866
L3vi <levistum@gmail.com>
2018-10-25 16:37:40 -0600

    "Updates..."

--- web/inClass/notes.php
+++ web/inClass/notes.php
@@ -1,8 +1,17 @@
 <?php
-$id = filter_input(INPUT_POST, 'id');
+require('dbConnect.php');
+$db = get_db();
+
+$course_id = filter_input(INPUT_POST, 'id');
 if ($id == NULL) {
 	$id = filter_input(INPUT_GET, 'id');
-}
+};
+
+$query = 'SELECT n.id AS note_id, c.code, c.name, n.content FROM note n JOIN course c ON n.course_id = c.id WHERE c.id=:course_id';
+$statement = $db->prepare($query);
+$statement->bindValue(':course_id', $course_id, PDO::PARAM_INT);
+$statement->execute();
+$notes = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 <!DOCTYPE html>
@@ -12,7 +21,9 @@ if ($id == NULL) {
 </head>
 <body>
 	<?php 
-	echo $id;
+	foreach ($notes as $note) {
+		echo $note;
+	}
 	?>
 </body>
 </html>
