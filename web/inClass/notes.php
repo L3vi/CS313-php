20a0d0f575e53cb630489d0a2a576ea050d18297
L3vi <levistum@gmail.com>
2018-10-25 16:25:31 -0600

    "Updates..."

--- web/inClass/notes.php
+++ web/inClass/notes.php
@@ -1,3 +1,18 @@
 <?php
-echo $id;
+$id = filter_input(INPUT_POST, 'id');
+if ($id == NULL) {
+	$id = filter_input(INPUT_GET, 'id');
+}
+
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Note</title>
+</head>
+<body>
+	<?php 
+	echo $id;
+	?>
+</body>
+</html>
