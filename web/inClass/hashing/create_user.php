5cede211f85770a2278764a4f7d55aeb16d2f61e
L3vi <levistum@gmail.com>
2018-10-30 16:37:30 -0600

    "Updates..."

--- web/inClass/hashing/create_user.php
+++ web/inClass/hashing/create_user.php
@@ -3,14 +3,14 @@
 require('dbConnnect.php');
 $db = get_db();
 
-$user = htmlspecialchars($_POST['username']);
+$username = htmlspecialchars($_POST['username']);
 $password = htmlspecialchars($_POST['password']);
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
 
 $statement = $db->prepare('INSERT INTO users (name, password) VALUES (:username, :password)');
-$statement->bindValue(':username', $user, PDO::PARAM_INT);
-$statement->bindValue(':password', $$hashedPassword, PDO::PARAM_INT);
+$statement->bindValue(':username', $username, PDO::PARAM_STR);
+$statement->bindValue(':password', $$hashedPassword, PDO::PARAM_STR);
 $statement->execute();
 
 ?>
