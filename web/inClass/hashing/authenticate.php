199daadebed995b6ec0c5020ee59a76ba3ee6dbb
L3vi <levistum@gmail.com>
2018-10-30 16:29:18 -0600

    "Updates..."

--- web/inClass/hashing/authenticate.php
+++ web/inClass/hashing/authenticate.php
@@ -1,5 +1,10 @@
 <?php
 
-// password_verify
+require('dbConnnect.php');
+$db = get_db();
+
+$user = htmlspecialchars($_POST['username']);
+$password = htmlspecialchars($_POST['password']);
+
 
 ?>
--- web/inClass/hashing/register.php
+++ web/inClass/hashing/register.php
@@ -11,7 +11,8 @@
 	<h1>Create an account</h1>
 	<form action="create_user.php" method="POST">
 		Username: <br/><input type="text" name="username"><br/>
-		Password: <br/><input type="text" name="password"><br/>
+		Password: <br/><input type="password" name="password"><br/>
+		<input type="submit">
 	</form>
 </body>
 </html>
