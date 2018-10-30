9ca28c94759b13c42eaba45c61e3d0896ec73fa5
L3vi <levistum@gmail.com>
2018-10-30 16:50:38 -0600

    "Updates..."

--- web/inClass/hashing/authenticate.php
+++ web/inClass/hashing/authenticate.php
@@ -7,7 +7,7 @@ $user = htmlspecialchars($_POST['username']);
 $password = htmlspecialchars($_POST['password']);
 
 
-$statement = $db->prepare('SELECT password FROM users WHERE name=:name');
+$statement = $db->prepare('SELECT password FROM users WHERE name=:user');
 $statement->bindValue(':user', $user, PDO::PARAM_STR);
 $statement->execute();
 $hashedPassword = $statement->fetch(PDO::FETCH_ASSOC);
