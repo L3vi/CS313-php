270ded337fb4abbe2e4371ae34bccd7179cb4829
L3vi <levistum@gmail.com>
2018-10-30 16:46:45 -0600

    "Updates..."

--- web/inClass/hashing/authenticate.php
+++ web/inClass/hashing/authenticate.php
@@ -11,6 +11,8 @@ $statement = $db->prepare('SELECT password FROM users');
 $statement->execute();
 $hashedPassword = $statement->fetch(PDO::FETCH_ASSOC);
 
+echo $password;
+echo $hashedPassword;
 
 if (password_verify($password, $hashedPassword['password'])) {
 	header('location:home.php');
