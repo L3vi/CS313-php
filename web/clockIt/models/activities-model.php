09680082dfaa2e684e35fd0414edc06eb3fae920
L3vi <levistum@gmail.com>
2018-11-06 04:23:40 -0700

    "Updates..."

--- web/clockIt/models/activities-model.php
+++ web/clockIt/models/activities-model.php
@@ -18,13 +18,11 @@ function createActivity($activityName) {
     // Create a connection object using the acme connection function
     $db = getDatabase();
     // The SQL statement
-    $sql = 'INSERT INTO activities (name)
-     VALUES (:activityName)';
+    $sql = 'INSERT INTO activities (name) VALUES (:activityName)';
     // Create the prepared statement using the acme connection
     $statement = $db->prepare($sql);
     // Bound values
     $statement->bindValue(':activityName', $activityName, PDO::PARAM_STR);
-    
     // Insert the data
     $statement->execute();
     // Ask how many rows changed as a result of our insert
