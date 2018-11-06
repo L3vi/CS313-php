69ff22caeee3f3d57b636caf2fa646f46539bb95
L3vi <levistum@gmail.com>
2018-11-06 03:56:37 -0700

    "Updates..."

--- web/clockIt/views/home.php
+++ web/clockIt/views/home.php
@@ -14,10 +14,7 @@ session_start();
         
         <nav class="navbar navbar-default bg-dark sticky-top">
             <a class="nav-link" href="activities/index.php?action=displayActivities">Activities</a>
-            <a class="nav-link" href="#">
-                <?php echo $activityName; ?>
-                    Work
-                </a>
+            <a class="nav-link" href="#"><?php echo $activityName; ?></a>
             <a class="nav-link" <?php if(isset($_SESSION['userId'])) { echo "href='accounts/index.php?action=account'"; } else { echo "href='accounts/index.php?action=loginView'"; }?>>Account</a>
         </nav>
 
