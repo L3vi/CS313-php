aa91facacf4ba345f42690edc49c1d0a723aac30
L3vi <levistum@gmail.com>
2018-11-06 04:11:49 -0700

    "Updates..."

--- web/clockIt/views/display-activities.php
+++ web/clockIt/views/display-activities.php
@@ -9,13 +9,13 @@
         <link rel="stylesheet" media="screen" href="../styling.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <meta name="viewport" content="width=device-width">
-        <title>Projects</title>
+        <title>Activities</title>
     </head>
 
     <!--    The body contains the actual bulk of the content.-->
     <body>
         <main>
-            <h1>Projects</h1>
+            <h1>Activities</h1>
 
             <!--Dynamically display activities-->
             <?php echo $activitiesList; ?>
@@ -24,7 +24,7 @@
                 <li>Service</li>
                 <li>Personal Project</li>
             </ul> -->
-            <a href="../activities/index.php?action=createActivityView">Create a new Project</a>
+            <a href="../activities/index.php?action=createActivityView">Create a new Activity</a>
 
 
         </main>
