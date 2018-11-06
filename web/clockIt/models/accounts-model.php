6dfc17608b776282ccf02aa2842c36cbc514c587
L3vi <levistum@gmail.com>
2018-11-06 02:18:19 -0700

    "Updates..."

--- web/clockIt/models/accounts-model.php
+++ web/clockIt/models/accounts-model.php
@@ -14,7 +14,9 @@ function loginUser($userEmail, $userPassword) {
     $user = $statement->fetch();
     $statement->closeCursor();
     var_dump($user);
+    var_dump($user[password]);
     $hashedPassword = $user[password];
+    echo $hashedPassword;
     return password_verify($userPassword, $hashedPassword);
 }
 
