ab712155b472a4378ced7e56f74c2c4646a01fe8
L3vi <levistum@gmail.com>
2018-11-28 01:36:51 -0700

    "Updates..."

--- web/clockIt/library/functions.php
+++ web/clockIt/library/functions.php
@@ -6,7 +6,10 @@ function createTimesheet($timeEntries) {
 	$timeSheet .= "<th scope='col'>Start Time</th>\n<th scope='col'>End Time</th>\n<th scope='col'>Total Time</th>\n<th scope='col'>Notes</th>";
 	$timeSheet .= "\n</tr>\n</thead>\n<tbody>";
 	foreach ($timeEntries as $entry) {
-		$timeSheet .= "<tr>\n<td>" . date('M-d-Y', strtotime($entry['starttime'])) . ' ' . date('h:i:s', strtotime($entry['starttime'])) . "</td>\n<td>" . date('M-d-Y', strtotime($entry['endtime'])) . ' ' . date('h:i:s', strtotime($entry['endtime'])) . "</td>\n<td></td>\n<td>$entry[notes]</td></tr>";
+		// Creates starting time
+		$timeSheet .= "<tr>\n<td>" . date('M-d-Y', strtotime($entry['starttime'])) . ' ' . date('h:i:s', strtotime($entry['starttime'])) . "</td>\n"
+		// Creates ending time
+		$timeSheet .= "<td>" . if(isset($entry['endtime']){date('M-d-Y', strtotime($entry['endtime'])) . ' ' . date('h:i:s', strtotime($entry['endtime']))} . "</td>\n<td></td>\n<td>$entry[notes]</td></tr>";
 	}
 	$timeSheet .= "\n</tbody>\n</table>";
 
